93a2970ad6a4b151460abac1cf58d01ffe33b802
Chandler Childs <rubberband75@gmail.com>
2024-04-24 12:02:47 -0600

    update static file url

--- src/shared/lib/hooks/box-redirect.ts
+++ src/shared/lib/hooks/box-redirect.ts
@@ -2,7 +2,7 @@ import { useParams } from './params';
 
 export const useBoxRedirect = () => {
   const params = useParams();
-  const BASE_LINK = `https://checkout.sollenaturals.com/?order_type_id=40&products%5B1094%5D=1`;
+  const BASE_LINK = `https://meadowlark.checkout.sollenaturals.com/?order_type_id=40&products%5B1094%5D=1`;
 
   if (!params) return BASE_LINK;
 
